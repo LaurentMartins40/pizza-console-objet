7c89fbe47027a3cf611561446946e13127dae6af
laurent <laurentmartins@outlook.com>
2019-02-08 15:16:32 +0100

    ajout du reste des javadocs

--- src/fr/pizzeria/service/ListerPizzaService.java
+++ src/fr/pizzeria/service/ListerPizzaService.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 
 import fr.pizzeria.dao.PizzaDao;
 import fr.pizzeria.model.Pizza;
-
+/**
+ * 
+ * @author Diginamic
+ *
+ */
 public class ListerPizzaService extends MenuService{
-
+	/**
+	 * Fonction du menu d'affichage des pizzas
+	 * @param pizzaDao type PizzaDao
+	 * @param scanner type Scanner
+	 */
 	@Override
 	public void executeUC(PizzaDao pizzaDao, Scanner scanner) {
 		System.out.println("Liste des pizzas");
